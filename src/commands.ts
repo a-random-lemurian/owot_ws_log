2dbcc5b2f60d6558f0dd3c8f865124eb18c805ca
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2024-05-27 09:27:10 +0800

    refactor: Reformat uptime duration format

--- src/commands.ts
+++ src/commands.ts
@@ -162,33 +162,17 @@ function traceback(ctx: cpr.CommandParserContext) {
 const startTime = new Date()
 
 function uptime(ctx: cpr.CommandParserContext) {
-    function timeAgo(givenDate: Date): string {
-        const currentDate = new Date();
-        const timeDifference = currentDate.getTime() - givenDate.getTime();
+    function durationStr(startDate: Date, endDate: Date) {
+        const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
     
-        const seconds = Math.floor(timeDifference / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-        const months = Math.floor(days / 30);
-        const years = Math.floor(days / 365);
-    
-        if (years >= 1) {
-            return `${years} years ago`;
-        } else if (months >= 1) {
-            return `${months} months ago`;
-        } else if (days >= 1) {
-            return `${days} days ago`;
-        } else if (hours >= 1) {
-            return `${hours} hours ago`;
-        } else if (minutes >= 1) {
-            return `${minutes} minutes ago`;
-        } else {
-            return `${seconds} seconds ago`;
-        }
+        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
     }
 
-    ctx.chat(`uptime: ${startTime.toISOString()} - ${timeAgo(startTime)}`)
+    ctx.chat(`uptime: ${startTime.toISOString()} - ${durationStr(startTime, new Date())}`)
 }
 
 function ffdr(ctx: cpr.CommandParserContext) {
