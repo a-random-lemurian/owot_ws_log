58aebbaefc974e540abff4482a856f351de7c253
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2024-05-27 08:23:10 +0800

    feat(cmds): Add `ch uptime` command

--- src/commands.ts
+++ src/commands.ts
@@ -153,6 +153,38 @@ function traceback(ctx: cpr.CommandParserContext) {
     ctx.chat("Check the console for the stacktrace, Lemuria.")
 }
 
+const startTime = new Date()
+
+function uptime(ctx: cpr.CommandParserContext) {
+    function timeAgo(givenDate: Date): string {
+        const currentDate = new Date();
+        const timeDifference = currentDate.getTime() - givenDate.getTime();
+    
+        const seconds = Math.floor(timeDifference / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(days / 365);
+    
+        if (years >= 1) {
+            return `${years} years ago`;
+        } else if (months >= 1) {
+            return `${months} months ago`;
+        } else if (days >= 1) {
+            return `${days} days ago`;
+        } else if (hours >= 1) {
+            return `${hours} hours ago`;
+        } else if (minutes >= 1) {
+            return `${minutes} minutes ago`;
+        } else {
+            return `${seconds} seconds ago`;
+        }
+    }
+
+    ctx.chat(`uptime: ${startTime.toISOString()} - ${timeAgo(startTime)}`)
+}
+
 function ffdr(ctx: cpr.CommandParserContext) {
     ctx.chat("<https://www.youtube.com/@FallingForDeadRosesGaming>");
 }
@@ -251,5 +283,10 @@ export const COMMANDS_LIST: cpr.Command[] = [
         restrictions: [
             cpr.CommandRestriction.TrustedUsersOnly
         ]
+    },
+    {
+        func: uptime,
+        name: "uptime",
+        helpInfo: "How long has the bot been up?"
     }
 ];
